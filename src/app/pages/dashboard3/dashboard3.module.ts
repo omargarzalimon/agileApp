3fdd77babc787b05f27e065319c52432bb6dafd3
Reyes <ireyesd1995@gmail.com>
2019-06-11 16:44:42 -0500

    Dasboard3
    Front de pantalla

---
+++ src/app/pages/dashboard3/dashboard3.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { Dashboard3Page } from './dashboard3.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: Dashboard3Page
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [Dashboard3Page]
+})
+export class Dashboard3PageModule {}
