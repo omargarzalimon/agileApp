1e852b843ea8a1441e052cbf9e96419b13dc14b1
Marisol Hernandez <karygrafika@gmail.com>
2019-06-17 11:28:07 -0500

    videos
    .

---
+++ src/app/pages/videoplay/videoplay.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-videoplay',
+  templateUrl: './videoplay.page.html',
+  styleUrls: ['./videoplay.page.scss'],
+})
+export class VideoplayPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
